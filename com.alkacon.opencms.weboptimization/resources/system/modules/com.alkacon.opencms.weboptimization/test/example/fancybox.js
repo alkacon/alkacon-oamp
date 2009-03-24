3eb6eeb9e99e61a6c72e225a338d4e50a7ccf335
mMoossen <m.moossen@alkacon.com>
2009-03-24 15:47:34 +0000

    added real world example

---
+++ com.alkacon.opencms.weboptimization/resources/system/modules/com.alkacon.opencms.weboptimization/test/example/fancybox.js
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<AlkaconOptimizedJss xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="opencms://system/modules/com.alkacon.opencms.weboptimization/schemas/optimized-js.xsd">
+  <AlkaconOptimizedJs language="en">
+    <Resource>
+      <Path>
+        <link type="WEAK">
+          <target><![CDATA[/system/modules/com.alkacon.opencms.weboptimization/test/jquery.fancybox/jquery-1.3.2.js]]></target>
+          <uuid>419c0632-1883-11de-bee7-e30a09549266</uuid>
+        </link>
+      </Path>
+    </Resource>
+    <Resource>
+      <Path>
+        <link type="WEAK">
+          <target><![CDATA[/system/modules/com.alkacon.opencms.weboptimization/test/jquery.fancybox/jquery.easing.1.3.js]]></target>
+          <uuid>cd669dd5-1878-11de-bee7-e30a09549266</uuid>
+        </link>
+      </Path>
+    </Resource>
+    <Resource>
+      <Path>
+        <link type="WEAK">
+          <target><![CDATA[/system/modules/com.alkacon.opencms.weboptimization/test/jquery.fancybox/jquery.fancybox-1.2.1.js]]></target>
+          <uuid>cd6899a8-1878-11de-bee7-e30a09549266</uuid>
+        </link>
+      </Path>
+    </Resource>
+  </AlkaconOptimizedJs>
+</AlkaconOptimizedJss>
