5a1c76c4524f8dff0678c47b0b723854ff7cd1b8
aWestermann <a.westermann@alkacon.com>
2009-04-17 07:29:54 +0000

    - wrong headers.

--- com.alkacon.opencms.formgenerator/src/com/alkacon/opencms/formgenerator/dialog/CmsFormEditDialog.java
+++ com.alkacon.opencms.formgenerator/src/com/alkacon/opencms/formgenerator/dialog/CmsFormEditDialog.java
@@ -1,32 +1,33 @@
 /*
  * File   : $Source: /alkacon/cvs/alkacon/com.alkacon.opencms.formgenerator/src/com/alkacon/opencms/formgenerator/dialog/CmsFormEditDialog.java,v $
- * Date   : $Date: 2008/05/16 10:09:43 $
- * Version: $Revision: 1.3 $
+ * Date   : $Date: 2009/04/17 07:29:54 $
+ * Version: $Revision: 1.4 $
  *
- * This library is part of OpenCms -
- * the Open Source Content Mananagement System
+ * This file is part of the Alkacon OpenCms Add-On Module Package
  *
- * Copyright (C) 2005 Alkacon Software GmbH (http://www.alkacon.com)
+ * Copyright (c) 2007 Alkacon Software GmbH (http://www.alkacon.com)
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * The Alkacon OpenCms Add-On Module Package is free software: 
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The Alkacon OpenCms Add-On Module Package is distributed 
+ * in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the Alkacon OpenCms Add-On Module Package.  
+ * If not, see http://www.gnu.org/licenses/.
  *
  * For further information about Alkacon Software GmbH, please see the
- * company website: http://www.alkacon.com
+ * company website: http://www.alkacon.com.
  *
  * For further information about OpenCms, please see the
- * project website: http://www.opencms.org
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * project website: http://www.opencms.org.
  */
 
 package com.alkacon.opencms.formgenerator.dialog;
@@ -60,9 +61,9 @@ import org.apache.commons.logging.Log;
 /**
  * Dialog to edit existing submitted data in the administration view.<p>
  * 
- * @author Anja Röttgers 
+ * @author Anja Roettgers 
  * 
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  * 
  * @since 7.0.4 
  */
